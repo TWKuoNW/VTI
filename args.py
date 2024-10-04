b0b61924b230027bc3d575952c269f2198ad3318
TWKuoNW <jp6jp642311@gmail.com>
2024-10-04 21:18:07 +0800

    VTI_Tool_V1

---
+++ args.py
@@ -0,0 +1,6 @@
+class args:
+    video_stream = "D:\VTI\2024-07-10 21-50-49.mp4" # 請修改成影片路徑
+    save_folder = "output" # 指定輸出資料夾
+    frames_per_capture = 10 # 指定多少幀取一張影像
+    img_format = "png"
+    
